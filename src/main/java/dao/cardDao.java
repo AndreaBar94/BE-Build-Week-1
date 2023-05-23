00f2026ceba094f0884d47b14d13e640d98a483b
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-05-23 17:25:28 +0200

    creazione nuovi metodi nel dao

--- src/main/java/dao/cardDao.java
+++ src/main/java/dao/cardDao.java
@@ -1,9 +1,11 @@
 package dao;
 
+import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 
 import org.slf4j.LoggerFactory;
 
@@ -19,14 +21,47 @@ public class cardDao {
 		this.em = em;
 	}
 	
+	public List<card> getAllCard() {
+		try {
+			TypedQuery<card> query = em.createQuery("SELECT c FROM card c", card.class);
+			List<card> cards = query.getResultList();
+			for(card c : cards) {
+				logger.info(c.toString());
+			};
+			return cards;
+		} catch (Exception e) {
+			logger.error("error: " + e);
+			throw e;
+		}
+		
+	};
 	
 	public void saveCard(card a) {
 		EntityTransaction t = em.getTransaction();
-		t.begin();
-		em.persist(a);
-		t.commit();
-		logger.info("Card saved!!");
+		try {
+			t.begin();
+			em.persist(a);
+			t.commit();
+			logger.info("Card saved!!");
+			
+		} catch (Exception e) {
+			logger.error("error: " + e);
+		}
 	}
+	
+	public void deleteCard(UUID id) {
+		EntityTransaction t = em.getTransaction();
+		
+		try {
+			t.begin();
+			card card = em.find(card.class, id);
+			em.remove(card);
+			t.commit();
+			logger.info("Removed Card");
+		} catch (Exception e) {
+			logger.error("error: " + e);
+		}
+	};
 	
 	public card findById(String id) {
 		card found = em.find(card.class, UUID.fromString(id));
